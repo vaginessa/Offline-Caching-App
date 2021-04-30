38a7d52ec143a1b3189c753dcf6232aa4bc6b730
Peter-cloud-web <petergichia165@gmail.com>
2021-04-30 04:55:33 +0300

    Add fields to the data class

--- app/src/main/java/com/example/restaurantsapp/Restaurant.kt
+++
@@ -1,4 +0,0 @@
-package com.example.restaurantsapp
-
-data class Restaurant {
-}
---
+++ app/src/main/java/com/example/restaurantsapp/data/Restaurant.kt
@@ -0,0 +1,7 @@
+package com.example.restaurantsapp.data
+
+data class Restaurant(
+    val name:String,
+    val type:String,
+    val logo:String,
+    val address:String)
