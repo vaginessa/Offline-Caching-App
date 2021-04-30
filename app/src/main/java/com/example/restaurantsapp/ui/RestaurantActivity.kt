215791d23eef334f24c1ae3e8cdcc88bd7fa4651
Peter-cloud-web <petergichia165@gmail.com>
2021-04-30 04:41:30 +0300

    design layout and add dependencies

--- app/src/main/java/com/example/restaurantsapp/ui/RestaurantActivity.kt
+++ app/src/main/java/com/example/restaurantsapp/ui/RestaurantActivity.kt
@@ -1,11 +1,12 @@
-package com.example.restaurantsapp
+package com.example.restaurantsapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.restaurantsapp.R
 
-class MainActivity : AppCompatActivity() {
+class RestaurantActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_restaurant)
     }
 }
