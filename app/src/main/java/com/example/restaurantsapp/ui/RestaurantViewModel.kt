60989a786abd2152be405fe73eee746d5f5ad530
Peter-cloud-web <petergichia165@gmail.com>
2021-05-01 05:57:56 +0300

    create viewmodel class to hold the business logic of the app

--- app/src/main/java/com/example/restaurantsapp/ui/RestaurantViewModel.kt
+++ app/src/main/java/com/example/restaurantsapp/ui/RestaurantViewModel.kt
@@ -1,4 +1,20 @@
 package com.example.restaurantsapp.ui
 
-class RestaurantViewModel {
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.restaurantsapp.api.ServiceBuilder
+import com.example.restaurantsapp.data.Restaurant
+import kotlinx.coroutines.launch
+
+class RestaurantViewModel : ViewModel() {
+    private val api = ServiceBuilder
+    private val restaurantsLiveData = MutableLiveData<List<Restaurant>>()
+    init {
+        viewModelScope.launch {
+            val restaurants = api.createRetrofitService()
+            restaurantsLiveData.value = restaurants
+        }
+    }
+
 }
