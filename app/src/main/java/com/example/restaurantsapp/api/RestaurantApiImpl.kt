5fe11560bba3e5755c10c7732bdd89f16e3d1dda
Peter-cloud-web <petergichia165@gmail.com>
2021-05-03 05:42:16 +0300

    getting restaurants as live data

---
+++ app/src/main/java/com/example/restaurantsapp/api/RestaurantApiImpl.kt
@@ -0,0 +1,6 @@
+package com.example.restaurantsapp.api
+
+abstract class RestaurantApiImpl {
+
+    abstract fun restaurantApi() : RestaurantApi
+}
--- app/src/main/java/com/example/restaurantsapp/ui/RestaurantViewModel.kt
+++ app/src/main/java/com/example/restaurantsapp/ui/RestaurantViewModel.kt
@@ -4,21 +4,20 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.restaurantsapp.api.RestaurantApi
+import com.example.restaurantsapp.api.RestaurantApiImpl
 import com.example.restaurantsapp.api.ServiceBuilder
 import com.example.restaurantsapp.data.Restaurant
 import kotlinx.coroutines.launch
 
-class RestaurantViewModel  : ViewModel(){
-    private val api = RestaurantApi.GR
-    private val restaurantsLiveData = MutableLiveData<List<Restaurant>>()
+class RestaurantViewModel(private val api:RestaurantApi)  : ViewModel(){
 
+    private val restaurantsLiveData = MutableLiveData<List<Restaurant>>()
 
     init {
         viewModelScope.launch {
-            val restaurants = api.
+            val restaurants = api.getRestaurants()
             restaurantsLiveData.value = restaurants
         }
     }
-
 
 }
