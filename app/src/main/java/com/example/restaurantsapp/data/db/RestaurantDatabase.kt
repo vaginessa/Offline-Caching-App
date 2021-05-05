028aa6d5b555f1cef5ca9287a098d5bd0d6d3fca
Peter-cloud-web <petergichia165@gmail.com>
2021-05-05 05:47:34 +0300

    build  database as main access point to relational database

--- app/src/main/java/com/example/restaurantsapp/data/db/RestaurantDatabase.kt
+++ app/src/main/java/com/example/restaurantsapp/data/db/RestaurantDatabase.kt
@@ -1,4 +1,20 @@
 package com.example.restaurantsapp.data.db
 
-abstract class RestaurantDatabase {
+import android.app.Application
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.restaurantsapp.data.Restaurant
+import com.example.restaurantsapp.data.dao.RestaurantDao
+
+@Database(entities = [Restaurant::class],version = 1)
+abstract class RestaurantDatabase : RoomDatabase() {
+
+    abstract fun restaurantDao(): RestaurantDao
+
+    private fun buildDatabase(app:Application) = Room.databaseBuilder(app,
+    RestaurantDatabase::class.java,"restaurant_database").build()
+
+
 }
