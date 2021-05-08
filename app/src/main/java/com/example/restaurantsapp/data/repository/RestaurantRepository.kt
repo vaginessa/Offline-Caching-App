cdf5852472f612566553d5086b2a4ad642158a59
Peter-cloud-web <petergichia165@gmail.com>
2021-05-08 03:49:08 +0300

    Implement methods in the networkbpound resource in the repository class

--- app/src/main/java/com/example/restaurantsapp/data/repository/RestaurantRepository.kt
+++ app/src/main/java/com/example/restaurantsapp/data/repository/RestaurantRepository.kt
@@ -1,4 +1,29 @@
 package com.example.restaurantsapp.data.repository
 
-class RestaurantRepository {
+import androidx.room.withTransaction
+import com.example.restaurantsapp.api.RestaurantApi
+import com.example.restaurantsapp.data.db.RestaurantDatabase
+import com.example.restaurantsapp.util.networkBoundResource
+import kotlinx.coroutines.delay
+
+class RestaurantRepository(private val api:RestaurantApi,private val db:RestaurantDatabase) {
+    private val restaurantDao = db.restaurantDao()
+
+    fun getRestaurants() = networkBoundResource(
+        query = {
+            restaurantDao.getAllRestaurants()
+        },
+        fetch = {
+            delay(2000)
+            api.getRestaurants()
+        },
+        saveFetchResult = { restaurants ->
+            db.withTransaction {
+                restaurantDao.deleteAllRestaurants()
+                restaurantDao.insertRestaurants(restaurants)
+            }
+
+        }
+    )
+
 }
